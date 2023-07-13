0faf4c0ff0e587d7da46f97118817d32a28373d7
g18s3628 <36521304+g18s3628@users.noreply.github.com>
2023-07-13 23:56:24 +0200

    optional_task2.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# optional_task2.py
---
+++ optional_task2.py
@@ -0,0 +1,9 @@
+#Request the user to input their favourite restuarant and store in string_fav
+string_fav = input("Enter your favourite restuarant name:")
+
+#Request the user to to input their favourite number 
+int_fav = int(input("Enter your number:"))
+
+#Print out the out of the user's request 
+print("Favourite restuarant:", string_fav) #Print out the the user's restuarants 
+print("Favourite number:", int_fav) #Print out user's favourite number 
